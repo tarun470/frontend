80a65e12179e7db8bba5e0e4951640f31eecb2ec
tarun470 <duggempuditarun6@gmail.com>
2025-12-13 13:13:57 +0530

    Improve Board click handling and disable invalid moves

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-
-export default function Board({ board, onClick }) {
+export default function Board({
+  board,
+  onClick,
+  disabled = false
+}) {
   return (
     <div className="board">
       {board.map((c, i) => (
-        <div key={i} className="cell" onClick={() => onClick(i)}>
+        <div
+          key={i}
+          className={`cell ${disabled || c ? "disabled" : ""}`}
+          onClick={() => {
+            if (!disabled && !c) {
+              onClick(i)
+            }
+          }}
+        >
           {c}
         </div>
       ))}
     </div>
   )
 }
+
